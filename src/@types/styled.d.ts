ca5624cd338fb6d9845b0fb9a8120d7b2a6543d4
Gabriel Maia <gribeiromaia736@gmail.com>
2024-01-08 20:51:30 -0300

    adding type definition on our theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+//O declare está criando uma tipagem de tema no styled comp.
+declare module 'styled-components'{
+  export interface DefaultTheme extends ThemeType{}
+}

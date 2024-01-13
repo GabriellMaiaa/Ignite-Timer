9078512e34502182826b1150cce14be8ea95da26
Gabriel Maia <gribeiromaia736@gmail.com>
2024-01-13 13:35:20 -0300

    starting the home page from figma

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { Home } from './pages/Home'
 
 export function Router() {
   return (
